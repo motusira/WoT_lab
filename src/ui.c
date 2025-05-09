e9b12d68d1c6075e85ce6b480167adfc928993bf
motusira <torusama@yandex.ru>
2025-05-09 12:42:42 +0300

    [фича] теперь выводится полная информация об игроке в поиске

--- src/ui.c
+++ src/ui.c
@@ -9,13 +9,14 @@
 UIButton *button, *find_button, *clear_button, *login_button;
 UILabel *label;
 UIWindow *window;
-UIPanel *login, *player_info, *pi_ui, *pi_result;
+UIPanel *login, *player_info, *pi_ui, *pi_result, *players_list;
 UITabPane *admin_pane;
 UITextbox *pi_input;
 
 void draw_info(PGconn *conn, const char *l) {
   const char *query =
-      "SELECT p.player_id, h.tank_id, ti.type, m.mod_id, h.game_points "
+      "SELECT p.player_id, p.status, p.currency_amount, p.total_damage, "
+      "p.destroyed_vehicles, h.tank_id, ti.type, m.mod_id, h.game_points "
       "FROM players p "
       "JOIN hangars h USING(player_id) "
       "JOIN tanks t USING(tank_id) "
@@ -42,8 +43,19 @@ void draw_info(PGconn *conn, const char *l) {
     return;
   }
 
-  snprintf(buff, 1024, "Player login: %s | Player ID: %s", l,
-           PQgetvalue(res, 0, 0));
+  snprintf(buff, 1024, "Login: %s", l);
+  UILabelCreate(&pi_result->e, 0, buff, -1);
+  snprintf(buff, 1024, "ID: %s", PQgetvalue(res, 0, 0));
+  UILabelCreate(&pi_result->e, 0, buff, -1);
+  snprintf(buff, 1024, "Status: %s", PQgetvalue(res, 0, 1));
+  UILabelCreate(&pi_result->e, 0, buff, -1);
+  snprintf(buff, 1024, "Currency amount: %s", PQgetvalue(res, 0, 2));
+  UILabelCreate(&pi_result->e, 0, buff, -1);
+  snprintf(buff, 1024, "Total damage: %s", PQgetvalue(res, 0, 3));
+  UILabelCreate(&pi_result->e, 0, buff, -1);
+  snprintf(buff, 1024, "Destroyed vehicles: %s", PQgetvalue(res, 0, 4));
+  UILabelCreate(&pi_result->e, 0, buff, -1);
+  snprintf(buff, 1024, "Currency amount: %s", PQgetvalue(res, 0, 2));
   UILabelCreate(&pi_result->e, 0, buff, -1);
   snprintf(buff, 1024, "| %-8s | %-12s | %-15s | %-10s |", "Tank ID", "Type",
            "Modification", "Points");
@@ -51,8 +63,8 @@ void draw_info(PGconn *conn, const char *l) {
 
   for (int i = 0; i < rows; i++) {
     snprintf(buff, 1024, "| %-8s | %-12s | %-15s | %-10s |",
-             PQgetvalue(res, i, 1), PQgetvalue(res, i, 2),
-             PQgetvalue(res, i, 3), PQgetvalue(res, i, 4));
+             PQgetvalue(res, i, 5), PQgetvalue(res, i, 6),
+             PQgetvalue(res, i, 7), PQgetvalue(res, i, 8));
     UILabelCreate(&pi_result->e, 0, buff, -1);
   }
 
@@ -78,14 +90,14 @@ int LoginButtonMessage(UIElement *element, UIMessage message, int di,
 
 int PIInputMessage(UIElement *element, UIMessage message, int di, void *dp) {
   if (message == UI_MSG_KEY_TYPED) {
-    UIKeyTyped *k = (UIKeyTyped *) dp;
-    if(k->code == UI_KEYCODE_ENTER) {
+    UIKeyTyped *k = (UIKeyTyped *)dp;
+    if (k->code == UI_KEYCODE_ENTER) {
       draw_info(pi_input->e.cp, UITextboxToCString(pi_input));
       UITextboxClear(pi_input, 0);
     }
-  } else if (message  == UI_MSG_USER) {
-      draw_info(pi_input->e.cp, UITextboxToCString(pi_input));
-      UITextboxClear(pi_input, 0);
+  } else if (message == UI_MSG_USER) {
+    draw_info(pi_input->e.cp, UITextboxToCString(pi_input));
+    UITextboxClear(pi_input, 0);
   }
   return 0;
 }
@@ -97,7 +109,8 @@ int FindButtonMessage(UIElement *element, UIMessage message, int di, void *dp) {
   return 0;
 }
 
-int ClearButtonMessage(UIElement *element, UIMessage message, int di, void *dp) {
+int ClearButtonMessage(UIElement *element, UIMessage message, int di,
+                       void *dp) {
   if (message == UI_MSG_CLICKED) {
     UIElementDestroyDescendents(&pi_result->e);
   }
@@ -114,11 +127,15 @@ void init(PGconn *conn) {
   login_button = UIButtonCreate(&login->e, 0, "Login", -1);
   login_button->e.messageUser = LoginButtonMessage;
   admin_pane = UITabPaneCreate(&window->e, 0, "Player info");
-  player_info = UIPanelCreate(&admin_pane->e, UI_PANEL_COLOR_1 | UI_PANEL_MEDIUM_SPACING);
-  pi_ui = UIPanelCreate(&player_info->e, UI_PANEL_COLOR_1 | UI_PANEL_HORIZONTAL);
-  pi_result = UIPanelCreate(&player_info->e, UI_PANEL_COLOR_1 | UI_PANEL_MEDIUM_SPACING | UI_PANEL_SCROLL);
+  player_info =
+      UIPanelCreate(&admin_pane->e, UI_PANEL_COLOR_1 | UI_PANEL_MEDIUM_SPACING |
+                                        UI_PANEL_SCROLL);
+  pi_ui =
+      UIPanelCreate(&player_info->e, UI_PANEL_COLOR_1 | UI_PANEL_HORIZONTAL);
+  pi_result = UIPanelCreate(&player_info->e,
+                            UI_PANEL_COLOR_1 | UI_PANEL_MEDIUM_SPACING);
   pi_input = UITextboxCreate(&pi_ui->e, 0);
-  pi_input->e.cp = (void *) conn;
+  pi_input->e.cp = (void *)conn;
   pi_input->e.messageUser = PIInputMessage;
   find_button = UIButtonCreate(&pi_ui->e, 0, "Find", -1);
   find_button->e.messageUser = FindButtonMessage;
